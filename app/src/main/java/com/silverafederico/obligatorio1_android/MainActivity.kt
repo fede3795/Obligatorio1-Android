7d7b23128c7d5c585e0075d0d2090b2cbfd2678b
fede3795 <fede_silver_10@hotmail.com>
2023-10-07 14:39:24 -0300

    mainactivity binding

--- app/src/main/java/com/silverafederico/obligatorio1_android/MainActivity.kt
+++ app/src/main/java/com/silverafederico/obligatorio1_android/MainActivity.kt
@@ -2,10 +2,13 @@ package com.silverafederico.obligatorio1_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.silverafederico.obligatorio1_android.databinding.LoginBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: LoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
