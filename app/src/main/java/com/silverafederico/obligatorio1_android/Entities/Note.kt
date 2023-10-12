b3095521fac911e4160be5c4ebf7565c50157d99
fede3795 <fede_silver_10@hotmail.com>
2023-10-11 22:36:04 -0300

    serializable

--- app/src/main/java/com/silverafederico/obligatorio1_android/Entities/Note.kt
+++ app/src/main/java/com/silverafederico/obligatorio1_android/Entities/Note.kt
@@ -1,8 +1,9 @@
 package com.silverafederico.obligatorio1_android.Entities
 
+import kotlinx.serialization.Serializable
 import java.util.Date
 
-
+@Serializable
 data class Note(
     var title: String,
     var description: String,
